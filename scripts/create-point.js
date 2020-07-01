3634ec6aa013acdbe52a44bddadfa0f46ebd73dd
Sergio Pastor Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-07-01 14:49:33 -0300

    New changes to the javascript

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -2,18 +2,22 @@
 function populateUFs()
 {
     const ufSelect = document.querySelector("select[name=uf]")
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/distritos").then(
-        (res) => { return res.json() }
-    ).then( states => {
-        
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/distritos")
+    .then( res => res.json() )
+    .then( states => {
+        for( const state of states )
+        {
+            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }
     })
 
 }
 
-
+populateUFs()
 
 
 document.querySelector("select[name=uf]")
 document.addEventListener("change", () => {
-    
+    console.log("mudei")
 })
