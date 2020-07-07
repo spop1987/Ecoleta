79f726f0f924934dce9475d7b1f4bf306eae5047
Sergio Pastor Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-07-07 14:39:02 -0300

    Hello world using the server

--- src/server.js
+++ src/server.js
@@ -2,5 +2,14 @@ const express = require("express")
 
 const server = express()
 
+// configurar caminhos da minha aplicacao
+// pagina inicial
+// req: requisiçaõ/ require
+// res: resposta/ response
+server.get("/", function(req, res){
+    res.send("Hello World")
+})
+
+
 // ligar o servidor
 server.listen(3000)
