883abbaaef16c714ff02dd52004f4243c5f68d1e
Sergio Pastor Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-07-07 15:08:26 -0300

    New actualizations to the server.js and the nodemon package was installed

--- src/server.js
+++ src/server.js
@@ -1,15 +1,21 @@
 const express = require("express")
 
 const server = express()
+// configurar pasta publica
+server.use(express.static("public"))
+
 
 // configurar caminhos da minha aplicacao
 // pagina inicial
 // req: requisiçaõ/ require
 // res: resposta/ response
 server.get("/", function(req, res){
-    res.send("Hello World")
+    // res.send("Hello World, How you doing?")
+    res.sendFile(__dirname + "/views/index.html")
 })
-
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
 
 // ligar o servidor
 server.listen(3000)
