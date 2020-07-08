010254ff847aa73e25bb3a3aa9302d4bc189afe0
Sergio Pastor Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-07-08 17:30:19 -0300

    It was updated the db.js archive and the db was exported to server.js

--- src/database/db.js
+++ src/database/db.js
@@ -5,75 +5,78 @@ const sqlite3 = require("sqlite3").verbose() // me entrega informações
 // criar o objeto que irá fazer operações no banco de dados
 const db = new sqlite3.Database("./src/database/database.db")
 
+module.exports = db
+
+
 // utilizar o objeto de banco de dados, para nossas operações
-db.serialize(() => {
-    // Com comandos sql:
+// db.serialize(() => {
+//     // Com comandos sql:
     
-    // 1. criar uma tabela
-    db.run(`
-            CREATE TABLE IF NOT EXISTS places (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                image TEXT,
-                name TEXT,
-                address TEXT,
-                address2 TEXT,
-                state TEXT,
-                city TEXT,
-                items TEXT
-            );
-    `)
-    // 2. inserir dados na tabela
-    const query = `
-           INSERT INTO places (
-                image, 
-                name,
-                address,
-                address2,
-                state,
-                city,
-                items
-            ) VALUES ( ?, ?, ?, ?, ?, ?, ? );
-    `
-    const values = [ 
-            "https://images.unsplash.com/photo-1551219059-b5f8e7acee56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-            "Colectoria",
-            "Guilherme Gemballa, Jardim América",
-            "Número 260",
-            "Santa Catarina",
-            "Rio do Sul",
-            "Resíduos Eletrônicos, Lâmpadas"
-    ]
+//     // 1. criar uma tabela
+//     db.run(`
+//             CREATE TABLE IF NOT EXISTS places (
+//                 id INTEGER PRIMARY KEY AUTOINCREMENT,
+//                 image TEXT,
+//                 name TEXT,
+//                 address TEXT,
+//                 address2 TEXT,
+//                 state TEXT,
+//                 city TEXT,
+//                 items TEXT
+//             );
+//     `)
+//     // 2. inserir dados na tabela
+//     const query = `
+//            INSERT INTO places (
+//                 image, 
+//                 name,
+//                 address,
+//                 address2,
+//                 state,
+//                 city,
+//                 items
+//             ) VALUES ( ?, ?, ?, ?, ?, ?, ? );
+//     `
+//     const values = [ 
+//             "https://images.unsplash.com/photo-1528323273322-d81458248d40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1602&q=80",
+//             "Papersider",
+//             "Guilherme Gemballa, Jardim América",
+//             "Número 280",
+//             "Santa Catarina",
+//             "Rio do Sul",
+//             "Papéis e Papelão"
+//     ]
     
-    function afterInsertData(error){
-        if(error)
-            return console.log(error)
+//     function afterInsertData(error){
+//         if(error)
+//             return console.log(error)
         
-        console.log("Cadastrado com sucesso")
-        console.log(this)
-    }
+//         console.log("Cadastrado com sucesso")
+//         console.log(this)
+//     }
     
-    // db.run(query, values, afterInsertData)
+//     db.run(query, values, afterInsertData)
 
     
-    // 3. deletar um dado da tabela
-    function afterDeleteData(error){
-        if(error)
-            return console.log(error)
+//     // 3. deletar um dado da tabela
+//     function afterDeleteData(error){
+//         if(error)
+//             return console.log(error)
 
-        console.log("Registro deletado com sucesso")
-    }
-    
-    db.run(`DELETE FROM places WHERE ID = ?`, [1], afterDeleteData)
+//         console.log("Registro deletado com sucesso")
+//     }
 
-    // 4. consultar os dados da tabela
+//     // db.run(`DELETE FROM places WHERE ID = ?`, [1], afterDeleteData)
 
-    function getData(error, rows){
-        if(error)
-            return console.log(error)
+//     // 4. consultar os dados da tabela
 
-        console.log("Aqui estão seus registros")
-        console.log(rows)
-    }
+//     function getData(error, rows){
+//         if(error)
+//             return console.log(error)
 
-    db.all(`SELECT * FROM places`, getData)
-})
+//         console.log("Aqui estão seus registros")
+//         console.log(rows)
+//     }
+
+//     db.all(`SELECT * FROM places`, getData)
+// })
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express")
 
 const server = express()
+
+// pegar o banco de dados
+const db = require("./database/db.js")
+
 // configurar pasta publica
 server.use(express.static("public"))
 
