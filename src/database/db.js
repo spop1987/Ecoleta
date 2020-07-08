185919525319115fff4688664d0e2e73eecf1829
Sergio Ontiveros Pérez <sergio.ontiveros.perez@outlook.com>
2020-07-08 17:03:12 -0300

    Update db.js
    It was updated the sql query code

--- src/database/db.js
+++ src/database/db.js
@@ -12,12 +12,49 @@ db.serialize(() => {
     // 1. criar uma tabela
     db.run(`
             CREATE TABLE IF NOT EXISTS places (
-                
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                image TEXT,
+                name TEXT,
+                address TEXT,
+                address2 TEXT,
+                state TEXT,
+                city TEXT,
+                items TEXT
             );
     `)
     // 2. inserir dados na tabela
+    const query = `
+           INSERT INTO places (
+                image, 
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+            ) VALUES ( ?, ?, ?, ?, ?, ?, ? );
+    `
+    const values = [ 
+            "https://images.unsplash.com/photo-1551219059-b5f8e7acee56?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+            "Colectoria",
+            "Guilherme Gemballa, Jardim América",
+            "Número 260",
+            "Santa Catarina",
+            "Rio do Sul",
+            "Resíduos Eletrônicos, Lâmpadas"
+    ]
+    
+    function afterInsertData(error){
+        if(error)
+            return console.log(error)
+        
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+    }
+    
+    db.run(query, values, afterInsertData)
 
     // 3. consultar os dados da tabela
-
+    
     // 4. deletar um dado da tabela
 })
